24ec5c2741f07a72dad7f8d528fc4209c8a4a8f0
A-Eren512 <eren_2003_akcay@hotmail.com>
2025-05-21 22:16:13 +0300

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import AllPharmacy from "./Pharmacy";
 import NobetciEczaneler from "./Pharmacy";
-import Code from "./QRCode";
-import App from "./yonlendir";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<NobetciEczaneler />);
+root.render(
+  <React.StrictMode>
+    <NobetciEczaneler />
+  </React.StrictMode>
+);
--- src/yonlendir.js
+++ src/yonlendir.js
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Code from "./QRCode";
-import AllPharmacy from "./Pharmacy";
+import NobetciEczaneler from "./Pharmacy";
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Code />} />
-        <Route path="/pharmacy" element={<AllPharmacy />} />
+        <Route path="/pharmacy" element={<NobetciEczaneler />} />
       </Routes>
     </Router>
   );
