5c1e1efb162080496a3cc83465b3da40f22c701b
A-Eren512 <eren_2003_akcay@hotmail.com>
2025-05-23 11:33:31 +0300

    feat: add image

--- src/Pharmacy.js
+++ src/Pharmacy.js
@@ -14,7 +14,6 @@ export default function NobetciEczaneler() {
       })
       .then((data) => {
         setEczaneler(data);
-        console.log(data);
         setLoading(false);
       })
       .catch((error) => {
@@ -34,23 +33,11 @@ export default function NobetciEczaneler() {
 
   if (hata) return <p>Hata: {hata}</p>;
 
-  const kendiBolgeEczaneleri = eczaneler.filter((e) => e.BolgeId === 42);
+  const kendiBolgeEczaneleri = eczaneler.filter((e) => e.BolgeId === 17);
   const kendiSecilenEczane =
     kendiBolgeEczaneleri.length > 0
       ? kendiBolgeEczaneleri.sort((a, b) => a.Adi.localeCompare(b.Adi))[0]
       : null;
-  const hedefBolgeler = [37, 65, 44, 58];
-  const digerBolgelerEczaneleri = hedefBolgeler.map((bolgeId) => {
-    const bolgedekiEczaneler = eczaneler.filter((e) => e.BolgeId === bolgeId);
-    const secilenEczane =
-      bolgedekiEczaneler.length > 0
-        ? bolgedekiEczaneler.sort((a, b) => a.Adi.localeCompare(b.Adi))[0]
-        : null;
-    return {
-      bolgeId,
-      eczane: secilenEczane,
-    };
-  });
 
   return (
     <div>
@@ -78,32 +65,6 @@ export default function NobetciEczaneler() {
               <div className="no-eczane-message">
                 BÖLGENİZDE NÖBETÇİ ECZANE BULUNAMADI.
               </div>
-            )}
-          </div>
-        </div>
-
-        <div className="accordion1">
-          <h2 className="section-title2">
-            Diğer Bölgelerdeki Nöbetçi Eczaneler
-          </h2>
-          <div className="accordion2">
-            {digerBolgelerEczaneleri.map(({ bolgeId, eczane }, index) =>
-              eczane ? (
-                <AccordionItemDigerBolge
-                  key={`diger-${index}`}
-                  title={eczane.Adi}
-                  latitude={eczane.LokasyonX}
-                  longitude={eczane.LokasyonY}
-                  adres={eczane.Adres}
-                  telefon={eczane.Telefon}
-                  bolgeId={eczane.BolgeId}
-                  bolge={eczane.Bolge}
-                />
-              ) : (
-                <div key={`bos-${index}`} className="no-eczane-message item2">
-                  BU BÖLGEDE NÖBETÇİ ECZANE BULUNAMADI.
-                </div>
-              )
             )}
           </div>
         </div>
@@ -153,53 +114,6 @@ function AccordionItemKendiBolge({
             style={{ fontSize: "15px", textAlign: "center", fontWeight: "600" }}
           >
             Konum için <br /> QR Kod
-          </p>
-        </div>
-      </div>
-    </div>
-  );
-}
-
-function AccordionItemDigerBolge({
-  title,
-  latitude,
-  longitude,
-  adres,
-  telefon,
-  bolge,
-}) {
-  const googleMapsUrl = `https://www.google.com/maps?q=${latitude},${longitude}`;
-  return (
-    <div className="item2 open">
-      <div className="image">
-        <p>
-          <img src="./images/pharmacy.png" alt="pharmacy-logo" />
-        </p>
-      </div>
-      <p className="text">{title}</p>
-      <p className="icon">-</p>
-      <div className="content-box2">
-        <div className="box2">
-          <p>
-            <strong>Adres:</strong> {adres}
-            <br />
-            <strong>Telefon:</strong> {telefon}
-            <br />
-            <strong>İlçe:</strong> {bolge}
-            <br />
-            <strong className="location">Enlem: {latitude}</strong>
-            <br />
-            <strong className="location">Boylam: {longitude}</strong>
-          </p>
-        </div>
-        <div className="qrCode2">
-          <QRCodeCanvas value={googleMapsUrl} size={128} />
-        </div>
-        <div className="font">
-          <p
-            style={{ fontSize: "18px", textAlign: "center", fontWeight: "600" }}
-          >
-            Konum için QR Kod
           </p>
         </div>
       </div>
